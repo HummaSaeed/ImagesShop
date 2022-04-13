bcc429280d91b900ea12c4f264c8248bedb10dab
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2022-04-13 11:41:19 +0500

    Screens code

---
+++ app/Components/AppButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet, View} from 'react-native';
+import AppColors from '../config/AppColors'
+
+
+function AppButton({title, color="primaryColor", onPress}) {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <View style={[styles.button,{backgroundColor:AppColors[color]}]}>
+                <Text style={styles.text}> {title}  </Text>
+            </View>
+        </TouchableOpacity>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    button:{
+        backgroundColor: AppColors.primaryColor,
+        borderRadius: 6,
+        width: 150,
+        padding: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin:5,
+        marginTop:20,
+    },
+    text:{
+        color: AppColors.white,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+})
+
+export default AppButton;
--- app/navigators/AuthNavigator.js
+++ app/navigators/AuthNavigator.js
@@ -7,17 +7,17 @@ import Screen2 from '../app/Screens/Screen2';
 import Screen3 from '../app/Screens/Screen3';
 import RegisterScreen from '../app/Screens/RegisterScreen';
 import LoginScreen from '../app/Screens/LoginScreen';
+import HomeScreen from '../app/Screens/Home'
 const AppStack =  createStackNavigator();
 
 
 const AuthNavigator = () => (
     <AppStack.Navigator presentation="modal">
-        <AppStack.Screen name="Home" component={Screen1}/>
+        <AppStack.Screen name="Home" component={Home}/>
         <AppStack.Screen name="Screen2" component={Screen2}/>
         <AppStack.Screen name="Screen3" component={Screen3}/>
         <AppStack.Screen name="RegisterScreen" component={RegisterScreen}/>
         <AppStack.Screen name="LoginScreen" component={LoginScreen}/>
-        {/* <AppStack.Screen name="HomeScreen" component={HomeScreen}/> */}
  
     </AppStack.Navigator>
 )
